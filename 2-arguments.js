20b25889855da8c06180e0046adfd7ba4a9a911e
BlessmanJay <blessmanjay@gmail.com>
2025-05-21 15:12:44 +0100

    Initial commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log("No arguments");
+  console.log("0 arguments");
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log("1 argument");
 } else {
   console.log("Arguments found");
 }
